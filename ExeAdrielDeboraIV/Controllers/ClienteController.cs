55ad80b8a302c265bba466d3ebbe6f3083a611e6
Massagli <adrielmassagli@gmail.com>
2023-06-19 00:44:08 -0300

    Information Working

---
+++ ExeAdrielDeboraIV/Controllers/ClienteController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExeAdrielDeboraIV.Models;
+
+namespace ExeAdrielDeboraIV.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Cadastrar()
+        {
+            ViewBag.Titulo = "Cadastre um Cliente";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Cliente cliente) 
+        {
+
+            
+            if (string.IsNullOrEmpty(cliente.NomeCLi))
+            {
+                ModelState.AddModelError("NomeCli", "O campo Nome é obrigatório");
+                return View(cliente);
+            }
+
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", cliente);
+            }
+
+            
+            return View();
+        }
+
+        public ActionResult Resultado(Cliente cliente)
+        {
+            ViewBag.Titulo = "Cliente cadastrado";
+            return View(cliente);
+        }
+
+        public ActionResult SelectCadastro(string login)
+        {
+            var dbExeAdrielDeboraIV = new Collection<string>
+            {
+                "Prático",
+                "Heitor",
+                "Cícero"
+
+            };
+            return Json(dbExeAdrielDeboraIV.All(a => a.ToLower() != login.ToLower()), JsonRequestBehavior.AllowGet);
+        }
+
+
+
+    }
+}
--- ExeAdrielDeboraIV/Models/Cliente.cs
+++ ExeAdrielDeboraIV/Models/Cliente.cs
@@ -3,33 +3,55 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Web;
+using System.Web.Mvc;
 
 namespace ExeAdrielDeboraIV.Models
 {
     public class Cliente
     {
 
-        [DisplayName("Código")]
+        [DisplayName("Código do Cliente")]
         [Range(1, 500, ErrorMessage = "O código deve ser maior que zero")]
         [Required(ErrorMessage = "O código é obrigatório")]
         public int IdCli { get; set; }
 
+
+        [DisplayName("Nome do Cliente")]
         [Required(ErrorMessage = "O Nome é obrigatório!")]
         public string NomeCLi { get; set; }
 
+        //[RegularExpression(@"([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2}))", 
+        //    ErrorMessage = "O campo deve conter no máximo 15 caracteres")]
         [Required(ErrorMessage = "Informe o CPF")]
         public int CPF { get; set; }
 
+
+        [EmailAddress]
+        [DisplayName("E_mail")]
         [Required(ErrorMessage = "O email é obrigatório")]
+        //[RegularExpression(@"/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i",
+        //    ErrorMessage = "Digite um email válido")]
         public string Email { get; set;}
 
+        [System.Web.Mvc.Remote("SelectCadastro", "Cliente", ErrorMessage = "Este Login já existe!")]
+        [RegularExpression(@"[a-zA-ZçÇ0-9]{6,15}",
+            ErrorMessage = "O campo deve conter no máximo 15 caracteres")]
         [Required(ErrorMessage = "O login é obrigatório")]
         public string Login { get; set;}
 
+        [DataType(DataType.Password)]
         [Required(ErrorMessage = "A senha é obrigatória")]
         public string Senha { get; set;}
 
+        [DataType(DataType.Password)]
+        [DisplayName("Confirme a senha")]
+        [System.Web.Mvc.Compare("Senha", ErrorMessage = "As senhas são diferentes.")]
+        public string ConfirmarSenha { get; set; }
+
+      
+        [DataType(DataType.Date)]
         public DateTime DataCadastro  { get; set;}
     }
 }
